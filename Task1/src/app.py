d75500ba265ebad00c177208010ea818cdad43d4
Hrishikesh <hrishikeshsukhdeo_darade@epam.com>
2025-05-28 11:27:16 +0530

    added request counts

--- Task1/src/app.py
+++ Task1/src/app.py
@@ -24,12 +24,14 @@ def submit():
                                  if current_time - timestamp < TIME_WINDOW]
 
     if len(request_counts[client_ip]) >= RATE_LIMIT:
+        request_counts[client_ip].append(current_time)
         return jsonify({'error': 'Rate limit exceeded. Try again later.',
                        'requests_in_window': len(request_counts[client_ip]) }), 429
 
   
     request_counts[client_ip].append(current_time)
-    return jsonify({'message': 'Request accepted.'}), 200
+    return jsonify({'message': 'Request accepted.',
+                    'current_requests': len(request_counts[client_ip])}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
